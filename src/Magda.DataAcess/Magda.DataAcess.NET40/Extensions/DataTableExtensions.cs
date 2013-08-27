00ce14feaa3e8b70c54f054093d63db1fed5f16b
Rajivhost <rajiv.mounguengue@hotmail.fr>
2013-08-27 16:18:25 +0100

    Initial check in.

---
+++ src/Magda.DataAcess/Magda.DataAcess.NET40/Extensions/DataTableExtensions.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------------------------------------------
+//  <copyright file="DataTableExtensions.cs" Company = "HSE">
+//    Copyright (c) 1987 - 2013 MUCODEC. All rights reserved.
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+using System.Data;
+using Catel;
+
+namespace Magda.DataAcess.Extensions
+{
+    public static class DataTableExtensions
+    {
+        public static void AddColumn<TColumn>(this DataTable dataTable, string columnName)
+        {
+            Argument.IsNotNull(() => dataTable);
+
+            dataTable.Columns.Add(columnName, typeof (TColumn));
+        }
+    }
+}
---
+++ src/Magda.DataAcess/Magda.DataAcess.NET40/Repositories/RepositoryBase.cs
@@ -0,0 +1,43 @@
+﻿// ------------------------------------------------------------------------------------------------
+//  <copyright file="RepositoryBase.cs" Company = "HSE">
+//    Copyright (c) 1987 - 2013 MUCODEC. All rights reserved.
+//  </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+using Catel;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace Magda.DataAcess
+{
+    public class RepositoryBase
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RepositoryBase" /> class.
+        /// </summary>
+        /// <param name="database">The database.</param>
+        public RepositoryBase(Database database)
+        {
+            Argument.IsNotNull(() => database);
+
+            Database = database;
+        }
+
+        #endregion
+
+        #region Properties
+
+        protected Database Database { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
